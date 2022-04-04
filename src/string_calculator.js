634ac7bb3a82161a08eb1e17a0d0bc5e129869c3
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-04-04 21:20:03 +0100

    solution kata string_calculator

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,15 +1,35 @@
-const parseDelimiter = (input) => {
-  const delimiter = numbers.match(/\/\/(.)\n/g);
-  if (delimiter.length) {
-    input = input.replace(newDelimiter[0], '');
-    input = input.replaceAll(newDelimiter[1], ',');
+const parseInput = (string) => {
+  const delimitersDefinition = string.match(/[\/]{2}(.+)\n/);
+  if (delimitersDefinition && delimitersDefinition[0]) {
+    // Suppression du motif de définition des délimiteurs
+    string = string.slice(delimitersDefinition[0].length);
+    const delimiters = [];
+    if (delimitersDefinition[1].length === 1) {
+      // Motif delimiteur d d'un caractère: //d\n
+      delimiters.push(delimitersDefinition[1]);
+    } else {
+      // Motif delimiteurs d1 d2 d3 de longueur variable : //[d1][d2][d3]\n
+      const delimitersIterator =
+        delimitersDefinition[1].matchAll(/\[([^\]]+)\]/g);
+      for (const [, delimiter] of delimitersIterator) {
+        delimiters.push(delimiter);
+      }
+    }
+    delimiters.forEach(
+      (delimiter) => (string = string.replaceAll(delimiter, ","))
+    );
   }
-  return input;
+  return string
+    .replaceAll("\n", ",")
+    .split(",")
+    .filter((num) => num <= 1000);
 };
 
 module.exports = (numbers) => {
-  return numbers
-    .replace('\n', ',')
-    .split(',')
-    .reduce((acc, val) => (acc += Number(val)), 0);
+  const normalizedArr = parseInput(numbers);
+  const negatives = normalizedArr.filter((num) => num < 0);
+  if (negatives?.length) {
+    throw new Error(`negatives numbers not allowed ${negatives.join(", ")}`);
+  }
+  return normalizedArr.reduce((acc, val) => (acc += Number(val)), 0);
 };
--- src/string_calculator.test.js
+++ src/string_calculator.test.js
@@ -1,32 +1,56 @@
-const { rules } = require('@typescript-eslint/eslint-plugin');
-const add = require('./string_calculator');
+const add = require("./string_calculator");
 
-test('Empty string', () => {
-  const result = add('');
+test("Empty string", () => {
+  const result = add("");
   expect(result).toBe(0);
 });
 
-test('One number', () => {
-  const result = add('1');
+test("One number", () => {
+  const result = add("1");
   expect(result).toBe(1);
 });
 
-test('Two numbers', () => {
-  const result = add('1,2');
+test("Two numbers", () => {
+  const result = add("1,2");
   expect(result).toBe(3);
 });
 
-test('Many numbers', () => {
-  const result = add('1,2,3,4');
+test("Many numbers", () => {
+  const result = add("1,2,3,4");
   expect(result).toBe(10);
 });
 
-test('Includes new lines character', () => {
-  const result = add('1\n2,3');
+test("Includes new lines character", () => {
+  const result = add("1\n2,3");
   expect(result).toBe(6);
 });
 
-test.only('New delimiter', () => {
-  const result = add('//;\n1;2');
+test("New delimiter", () => {
+  const result = add("//;\n1;2");
   expect(result).toBe(3);
+});
+
+test("Fails with one negative number", () => {
+  expect(() => add("-1,1")).toThrow("negatives numbers not allowed -1");
+});
+
+test("Fails with many negatives number", () => {
+  expect(() => add("-1,-2,-3")).toThrow(
+    "negatives numbers not allowed -1, -2, -3"
+  );
+});
+
+test("Ignore numbers highers than 1000", () => {
+  const result = add("2,1001");
+  expect(result).toBe(2);
+});
+
+test("Allow delimiters of any length", () => {
+  const result = add("//[***]\n1***2***3");
+  expect(result).toBe(6);
+});
+
+test("Allow multiple delimiters", () => {
+  const result = add("//[*][%]\n1*2%3");
+  expect(result).toBe(6);
 });
