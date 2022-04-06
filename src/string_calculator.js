cd5bfe47e25b586d2b8ba8245826ddca90ab3c35
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-04-06 20:14:52 +0100

    kata encode

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,23 +1,28 @@
-const parseInput = (string) => {
+const normalizeDelimiters = (string) => {
+  // Suppression du motif de définition des délimiteurs
+  string = string.slice(delimitersDefinition[0].length);
+  const delimiters = [];
+  if (delimitersDefinition[1].length === 1) {
+    // Motif delimiteur d d'un caractère: //d\n
+    delimiters.push(delimitersDefinition[1]);
+  } else {
+    // Motif delimiteurs d1 d2 d3 de longueur variable : //[d1][d2][d3]\n
+    const delimitersIterator =
+      delimitersDefinition[1].matchAll(/\[([^\]]+)\]/g);
+    for (const [, delimiter] of delimitersIterator) {
+      delimiters.push(delimiter);
+    }
+  }
+  delimiters.forEach(
+    (delimiter) => (string = string.replaceAll(delimiter, ","))
+  );
+  return string;
+};
+
+const getNumbersArr = (string) => {
   const delimitersDefinition = string.match(/[\/]{2}(.+)\n/);
   if (delimitersDefinition && delimitersDefinition[0]) {
-    // Suppression du motif de définition des délimiteurs
-    string = string.slice(delimitersDefinition[0].length);
-    const delimiters = [];
-    if (delimitersDefinition[1].length === 1) {
-      // Motif delimiteur d d'un caractère: //d\n
-      delimiters.push(delimitersDefinition[1]);
-    } else {
-      // Motif delimiteurs d1 d2 d3 de longueur variable : //[d1][d2][d3]\n
-      const delimitersIterator =
-        delimitersDefinition[1].matchAll(/\[([^\]]+)\]/g);
-      for (const [, delimiter] of delimitersIterator) {
-        delimiters.push(delimiter);
-      }
-    }
-    delimiters.forEach(
-      (delimiter) => (string = string.replaceAll(delimiter, ","))
-    );
+    string = normalizeDelimiters(string);
   }
   return string
     .replaceAll("\n", ",")
@@ -26,7 +31,7 @@ const parseInput = (string) => {
 };
 
 module.exports = (numbers) => {
-  const normalizedArr = parseInput(numbers);
+  const normalizedArr = getNumbersArr(numbers);
   const negatives = normalizedArr.filter((num) => num < 0);
   if (negatives?.length) {
     throw new Error(`negatives numbers not allowed ${negatives.join(", ")}`);
